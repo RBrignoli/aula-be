c9ca807010f1a2420fbb14562793e489f7ccae8e
daniloamorim <professor@SN-363526.edu.fiesc.com.br>
2024-08-05 16:11:07 -0300

    adicionando rota de create

--- index.js
+++ index.js
@@ -1,19 +1,47 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const { v4: uuidv4 } = require('uuid')
+const fs = require('fs')
 
 app.use(bodyParser.json())
 
 const db = require('./db.json')
+const { error } = require('console')
+
 app.get('/produtos', function (req,res) {
     var produtos = db.produtos
     res.json(produtos)
 })
 app.get('/produtos/:id', function (req, res){
     const _id = req.params.id
-    console.log(db.produtos)
-    res.send(_id)
-    // filtrar lista de produtos e retornar no res.send() apenas o produto do id selecionado
+    const lista_produtos = db.produtos
+    const produto = lista_produtos.find(
+        (produto) => produto.id == _id
+        )
+    produto ? res.send(produto) : res.status(404).send({error:'not found'})
+
+})
+app.post('/produtos', function (req,res){
+    const dados = req.body
+    if(!dados.nome || !dados.preco) {
+        res.status(406).send({error:'Nome e preço deve ser informado'})
+    }
+    const _id = uuidv4()
+    dados.id = _id
+    
+    lista_produtos = db.produtos
+    lista_produtos.push(dados)
+    console.log(lista_produtos)
+    fs.writeFile('./db.json', JSON.stringify(lista_produtos), (err) => {
+        if (err){
+            res.status(500).send({error:'erro no servidor'})
+        }
+    })
+
+    //salvar o novo produto na lista de produtos
+    //retornar uma resposta de sucesso ou de erro para o usuário
+    res.json(dados)
 })
 
 app.listen(8000)
